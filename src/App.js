44569cfedc6aa5b9382bde51aea0434f7fb1cf4a
hiroki nakahashi <hirofighter@gmail.com>
2021-09-02 15:08:19 +0900

    prop-type

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-function App() {
+import { PROPERTY_TYPES } from '@babel/types';
+import React from 'react';
+
+const App = () => {
+  const profiles = [
+    { name: 'Taro', age: 10 },
+    { name: 'Hanako', age: 15 },
+    { name: 'NoName', age: 3 },
+  ];
   return (
     <div>
-      <h1>Hello World!</h1>
+      {profiles.map((profile, index) => {
+        return <User name={profile.name} age={profile.age} key={index} />;
+      })}
     </div>
   );
-}
+};
+
+const User = (props) => {
+  return (
+    <div>
+      Hi, I am {props.name} age={props.age} years old!
+    </div>
+  );
+};
+
+User.propType = {
+  name: PROPERTY_TYPES.string,
+  age: PROPERTY_TYPES.number,
+};
 
 export default App;
