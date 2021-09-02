d1b718c62caf9335d11db47a74d91b79a75d2e9c
hiroki nakahashi <hirofighter@gmail.com>
2021-09-02 15:26:50 +0900

    Learn state

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
-import { PROPERTY_TYPES } from '@babel/types';
-import React from 'react';
+import React, { Component } from 'react';
 
-const App = () => {
-  const profiles = [
-    { name: 'Taro', age: 10 },
-    { name: 'Hanako', age: 15 },
-    { name: 'NoName', age: 3 },
-  ];
-  return (
-    <div>
-      {profiles.map((profile, index) => {
-        return <User name={profile.name} age={profile.age} key={index} />;
-      })}
-    </div>
-  );
-};
+const App = () => <Counter></Counter>;
 
-const User = (props) => {
-  return (
-    <div>
-      Hi, I am {props.name} age={props.age} years old!
-    </div>
-  );
-};
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
 
-User.propType = {
-  name: PROPERTY_TYPES.string,
-  age: PROPERTY_TYPES.number,
-};
+  handlePlusButton = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  handleMinusButton = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+  render() {
+    return (
+      <>
+        <div>count: {this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </>
+    );
+  }
+}
 
 export default App;
